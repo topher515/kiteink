22508cce400f5bf04cf534fb95f08d5fd732be56
Chris Wilcox <ckwilcox@gmail.com>
2022-02-17 17:15:59 -1000

    cleanup

--- remote-refresh.sh
+++ remote-refresh.sh
@@ -1,4 +1,4 @@
 
 #!/usr/bin/env bash
 
-ssh -t pi@$RPI_IP "WF_USERNAME='$WF_USERNAME' WF_PASSWORD='$WF_PASSWORD' cd /home/pi/app && PIPENV_VENV_IN_PROJECT=1 pipenv run scripts/fetch_and_epd_display_latest_favorites.sh"
+ssh -t pi@$RPI_IP "cd /home/pi/app && PIPENV_VENV_IN_PROJECT=1 WF_USERNAME='$WF_USERNAME' WF_PASSWORD='$WF_PASSWORD' pipenv run scripts/fetch_and_epd_display_latest_favorites.sh"

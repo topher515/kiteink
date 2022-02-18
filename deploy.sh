ae5b261d016f585ce01dc1617bcad7413a56636c
Chris Wilcox <ckwilcox@gmail.com>
2022-02-17 16:02:44 -1000

    dont use tar

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 # Deploy current project to RPi
 
-tar czf dist/kiteink.tgz Pipfile Pipfile.lock weather-reporter-package scripts && \
-    ssh pi@$RPI_IP "rm -rf /tmp/app/ && mkdir -p /tmp/app" && \
-    scp dist/kiteink.tgz pi@$RPI_IP:/tmp/app/kiteink.tgz && \
-    ssh pi@$RPI_IP "cd /tmp/app && tar xfz kiteink.tgz && rm kiteink.tgz && PIPENV_VENV_IN_PROJECT=1 pipenv install"
+# tar -czf dist/kiteink.tgz Pipfile Pipfile.lock weather-reporter-package scripts && \
+ssh pi@$RPI_IP "rm -rf /tmp/app/ && mkdir -p /tmp/app" && \
+    scp -r Pipfile Pipfile.lock weather-reporter-package scripts pi@$RPI_IP:/tmp/app/ && \
+    ssh pi@$RPI_IP "cd /tmp/app && PIPENV_VENV_IN_PROJECT=1 pipenv install"
 
